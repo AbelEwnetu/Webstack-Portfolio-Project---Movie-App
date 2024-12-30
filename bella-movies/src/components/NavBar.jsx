cea4577de138bd5cddebe7d4a1f376c41828dc1b
AbelEwnetu <ebeloscom@gmail.com>
2024-12-30 19:09:52 +0300

    importing NavBar in my App.jsx

--- bella-movies/src/components/NavBar.jsx
+++ bella-movies/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/">Bella Movies</Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
