6f2339deb0140d541f87dd3c2ab44020a1d377db
AbelEwnetu <ebeloscom@gmail.com>
2024-12-30 17:57:21 +0300

    using stateto handle dynamic parts of my component

--- bella-movies/src/pages/Home.jsx
+++ bella-movies/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
     const movies = [
         {id: 1, title: "John Wick", release_date: "2020"},
         {id: 2, title: "Terminator", release_date: "1999"},
         {id: 3, title: "The Matrix", release_date: "1998"},
     ];
-    const handleSearch = () => {};
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    };
         
 
 
@@ -15,7 +21,10 @@ function Home() {
         <form onSubmit={handleSearch} className="search-form">
             <input type="text"
             placeholder="Search for movies..."
-            className="search-input"/>
+            className="search-input"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            />
             <button type="submit" className="search-button">Search</button>
         </form>
 
